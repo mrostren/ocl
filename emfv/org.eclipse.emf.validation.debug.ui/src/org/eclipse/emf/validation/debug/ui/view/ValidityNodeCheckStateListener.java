d83acc4a20a9e1ed54126c7c9bf6d0e32ae3b4ab
marwa rostren <mrostren@obeo.fr>
2014-01-23 15:43:22 +0100

    Add nodes selection facilities
    - deselect and disable All ResultNodes when the direct parentNode is
    deselected,
    - deselect and disable the parentNode if all its children are deselected
    - select and enable the parentNode if all its children are selected
    - select and enable ancestors if one leaf has been selected 
    
    
    Change-Id: I91d184b589660f6991037a8eb0ffe9d4ed4015f9
    Signed-off-by: marwa rostren <mrostren@obeo.fr>

--- emfv/org.eclipse.emf.validation.debug.ui/src/org/eclipse/emf/validation/debug/ui/view/ValidityNodeCheckStateListener.java
+++ emfv/org.eclipse.emf.validation.debug.ui/src/org/eclipse/emf/validation/debug/ui/view/ValidityNodeCheckStateListener.java
@@ -9,6 +9,7 @@
  *
  * Contributors:
  *	E.D.Willink (CEA LIST) - initial API and implementation
+ *	Obeo - Add selection facilities
  *
  * </copyright>
  */
@@ -19,6 +20,9 @@ import java.util.List;
 
 import org.eclipse.emf.validation.debug.validity.AbstractNode;
 import org.eclipse.emf.validation.debug.validity.ConstrainingNode;
+import org.eclipse.emf.validation.debug.validity.LeafConstrainingNode;
+import org.eclipse.emf.validation.debug.validity.ResultConstrainingNode;
+import org.eclipse.emf.validation.debug.validity.ResultValidatableNode;
 import org.eclipse.emf.validation.debug.validity.RootConstrainingNode;
 import org.eclipse.emf.validation.debug.validity.RootValidatableNode;
 import org.eclipse.emf.validation.debug.validity.ValidatableNode;
@@ -45,15 +49,18 @@ public class ValidityNodeCheckStateListener implements ICheckStateListener {
 			// update Selected Element check
 			abstractNode.setEnabled(checked);
 
+			//update Selected Element Results children check;
+			updateResultNodeState(abstractNode, checked);
+
 			// update Selected Element parents checks/grayed
 			if (abstractNode instanceof RootValidatableNode || abstractNode instanceof RootConstrainingNode){
 				updateRootNode(abstractNode);					
 			} else if (abstractNode instanceof ConstrainingNode) {
 				ConstrainingNode constrainingNode = (ConstrainingNode) abstractNode;
-				updateConstrainingNodeAncestors(constrainingNode);
+				updateConstrainingNodeAncestors(constrainingNode, checked);
 			} else if (abstractNode instanceof ValidatableNode) {
 				ValidatableNode validatableNode = (ValidatableNode) abstractNode;
-				updateValidatableNodeAncestors(validatableNode);
+				updateValidatableNodeAncestors(validatableNode, checked);
 			}
 		} else {
 			// DO NOTHING
@@ -86,21 +93,31 @@ public class ValidityNodeCheckStateListener implements ICheckStateListener {
 		}
 	}
 	
-	private void updateConstrainingNodeAncestors(ConstrainingNode child) {
+	private void updateConstrainingNodeAncestors(ConstrainingNode child, boolean checked) {
 		ConstrainingNode parent = child.getParent();
 		if (parent != null) {
+			//Enable/disable the parent if all children are enabled/disabled
+			if (checked && !parent.isAllChildrenDisabled() || !checked && parent.isAllChildrenEnabled()){
+				parent.setEnabled(checked);
+				constraintsTree.setChecked(parent, checked);
+			} else if (!checked && parent.isAllChildrenDisabled()){
+				parent.setEnabled(checked);
+				constraintsTree.setChecked(parent, checked);
+			}
 			List<ConstrainingNode> checkedChildren = getCheckedConstrainingNodeChildren(parent);
-			if (checkedChildren.size() == parent.getChildren().size()) {
-				List<ConstrainingNode> grayedChildren = getGrayedConstrainingNodeChildren(parent);
-				if (grayedChildren.size() == 0) {
-					constraintsTree.setGrayed(parent, false);
+			if (parent.isEnabled()){
+				if (checkedChildren.size() == parent.getChildren().size()) {
+					List<ConstrainingNode> grayedChildren = getGrayedConstrainingNodeChildren(parent);
+					if (grayedChildren.size() == 0) {
+						constraintsTree.setGrayed(parent, false);
+					} else {
+						constraintsTree.setGrayed(parent, true);
+					}
 				} else {
 					constraintsTree.setGrayed(parent, true);
 				}
-			} else {
-				constraintsTree.setGrayed(parent, true);
 			}
-			updateConstrainingNodeAncestors(parent);
+			updateConstrainingNodeAncestors(parent, checked);
 		}
 	}
 
@@ -165,21 +182,31 @@ public class ValidityNodeCheckStateListener implements ICheckStateListener {
 		return grayedChildren;
 	}
 
-	private void updateValidatableNodeAncestors(ValidatableNode child) {
+	private void updateValidatableNodeAncestors(ValidatableNode child, boolean checked) {
 		ValidatableNode parent = child.getParent();
 		if (parent != null) {
+			//Enable/disable the parent if all children are enabled/disabled
+			if (checked && !parent.isAllChildrenDisabled() || !checked && parent.isAllChildrenEnabled()){
+				parent.setEnabled(checked);
+				validatableTree.setChecked(parent, checked);
+			} else if (!checked && parent.isAllChildrenDisabled()){
+				parent.setEnabled(checked);
+				validatableTree.setChecked(parent, checked);
+			}
 			List<ValidatableNode> checkedChildren = getCheckedValidatableNodeChildren(parent);
-			if (checkedChildren.size() == parent.getChildren().size()) {
-				List<ValidatableNode> grayedChildren = getGrayedValidatableNodeChildren(parent);
-				if (grayedChildren.size() == 0) {
-					validatableTree.setGrayed(parent, false);
+			if (parent.isEnabled()){
+				if (checkedChildren.size() == parent.getChildren().size()) {
+					List<ValidatableNode> grayedChildren = getGrayedValidatableNodeChildren(parent);
+					if (grayedChildren.size() == 0) {
+						validatableTree.setGrayed(parent, false);
+					} else {
+						validatableTree.setGrayed(parent, true);
+					}
 				} else {
 					validatableTree.setGrayed(parent, true);
 				}
-			} else {
-				validatableTree.setGrayed(parent, true);
 			}
-			updateValidatableNodeAncestors(parent);
+			updateValidatableNodeAncestors(parent, checked);
 		}
 	}
 
@@ -203,5 +230,26 @@ public class ValidityNodeCheckStateListener implements ICheckStateListener {
 			}
 		}
 		return grayedChildren;
+	}
+	/**
+	 * Select/Deselect all results children nodes
+	 */
+	private void updateResultNodeState(AbstractNode abstractNode,
+			boolean checked) {
+		for (AbstractNode child : abstractNode.getChildren()) {
+			if (child instanceof ResultValidatableNode) {
+				child.setEnabled(checked);
+				validatableTree.setChecked(child, checked);
+				validatableTree.setGrayed(child, false);
+			} else if (child instanceof LeafConstrainingNode) {
+				child.setEnabled(checked);
+				constraintsTree.setChecked(child, checked);
+				updateResultNodeState(child, checked);
+			} else if (child instanceof ResultConstrainingNode) {
+				child.setEnabled(checked);
+				constraintsTree.setChecked(child, checked);
+				constraintsTree.setGrayed(child, false);
+			}
+		}
 	}
 }
